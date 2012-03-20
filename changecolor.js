392275edff7e4e6f3b765bb9b44b44e93fe3ffa5
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-21 00:38:07 +0100

    Tried to make it more readable

--- changecolor.js
+++ changecolor.js
@@ -10,6 +10,8 @@ changecolor = (function() {
     };
 
     function doit(color, ratio, darker) {
+        var difference, rgb, alpha, decimal, returnValue;
+
         if (!color) return color;
 
         // Trim trailing/leading whitespace
@@ -19,29 +21,53 @@ changecolor = (function() {
         color = color.replace(/^#?([a-f0-9])([a-f0-9])([a-f0-9])$/i, '#$1$1$2$2$3$3');
 
         // Calculate ratio
-        var difference = Math.round(ratio * 256) * (darker ? - 1: 1),
+        difference = Math.round(ratio * 256) * (darker ? - 1: 1);
+
         // Determine if input is RGB(A)
-        rgb = color.match(new RegExp('^rgba?\\(\\s*' + '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' + '\\s*,\\s*' + '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' + '\\s*,\\s*' + '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' + '(?:\\s*,\\s*' + '(0|1|0?\\.\\d+))?' + '\\s*\\)$', 'i')),
-        alpha = !! rgb && rgb[4] != null ? rgb[4] : null,
+        rgb = color.match(new RegExp('^rgba?\\(\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '\\s*,\\s*' + 
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '\\s*,\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '(?:\\s*,\\s*' +
+            '(0|1|0?\\.\\d+))?' +
+            '\\s*\\)$', 'i'));
+
+        if (!!rgb && rgb[4] !== null) alpha = rgb[4];
+        else alpha = null;
 
         // Convert hex to decimal
-        decimal = !! rgb ? [rgb[1], rgb[2], rgb[3]] : color.replace(
+        if (!!rgb) {
+            decimal = [rgb[1], rgb[2], rgb[3]];
+        } else {
+            decimal = color.replace(/^#?([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])/i, function() {
+                return parseInt(arguments[1], 16) +
+                    ',' +
+                    parseInt(arguments[2], 16) +
+                    ',' +
+                    parseInt(arguments[3], 16);
+            }).split(/,/);
+        }
 
-        /^#?([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])/i, function() {
-            return parseInt(arguments[1], 16) + ',' + parseInt(arguments[2], 16) + ',' + parseInt(arguments[3], 16);
-        }).split(/,/),
-        returnValue;
-
-        // Return RGB(A)
-        return !! rgb ? 'rgb' + (alpha !== null ? 'a': '') + '(' + Math[darker ? 'max': 'min'](
-        parseInt(decimal[0], 10) + difference, darker ? 0: 255) + ', ' + Math[darker ? 'max': 'min'](
-        parseInt(decimal[1], 10) + difference, darker ? 0: 255) + ', ' + Math[darker ? 'max': 'min'](
-        parseInt(decimal[2], 10) + difference, darker ? 0: 255) + (alpha !== null ? ', ' + alpha: '') + ')':
-        // Return hex
-        ['#', pad(Math[darker ? 'max': 'min'](
-        parseInt(decimal[0], 10) + difference, darker ? 0: 255).toString(16), 2), pad(Math[darker ? 'max': 'min'](
-        parseInt(decimal[1], 10) + difference, darker ? 0: 255).toString(16), 2), pad(Math[darker ? 'max': 'min'](
-        parseInt(decimal[2], 10) + difference, darker ? 0: 255).toString(16), 2)].join('');
+        if (!!rgb) {
+            // Return RGB(A)
+            return 'rgb' + (alpha !== null ? 'a': '') + '(' +
+                Math[darker ? 'max': 'min'](parseInt(decimal[0], 10) + difference, darker ? 0: 255) + ', ' +
+                Math[darker ? 'max': 'min'](parseInt(decimal[1], 10) + difference, darker ? 0: 255) + ', ' +
+                Math[darker ? 'max': 'min'](parseInt(decimal[2], 10) + difference, darker ? 0: 255) +
+                (alpha !== null ? ', ' + alpha: '') + ')';
+        } else {
+            // Return hex
+            return ['#', 
+                    pad(Math[darker ? 'max': 'min'](
+                        parseInt(decimal[0], 10) + difference, darker ? 0: 255).toString(16), 2), 
+                    pad(Math[darker ? 'max': 'min'](
+                        parseInt(decimal[1], 10) + difference, darker ? 0: 255).toString(16), 2), 
+                    pad(Math[darker ? 'max': 'min'](
+                        parseInt(decimal[2], 10) + difference, darker ? 0: 255).toString(16), 2)
+                ].join('');
+        }
     }
 
     return {
